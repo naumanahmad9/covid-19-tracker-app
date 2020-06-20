4220407b07a28dcdaf164e47e48a5e17c8201c2e
Nauman Ahmad <tnsd995@gmail.com>
2020-06-20 23:29:34 +0500

    made changes in packages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { Cards, CountryPicker, Chart } from "./components";
+import { fetchData } from "./api/";
+import styles from "./App.module.css";
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+import image from "./images/image.png";
+
+class App extends Component {
+  state = {
+    data: {},
+    country: "",
+  };
+
+  async componentDidMount() {
+    const data = await fetchData();
+
+    this.setState({ data });
+  }
+
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country });
+  };
+
+  render() {
+    const { data, country } = this.state;
+
+    return (
+      <div className={styles.container}>
+        <img className={styles.image} src={image} alt="COVID-19" />
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
+      </div>
+    );
+  }
 }
-
 export default App;
